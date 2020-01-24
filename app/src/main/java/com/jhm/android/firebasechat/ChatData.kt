f05768fb5eae810a64b7541c84c0cb6e4bf31907
J-HM <als091212@naver.com>
2020-01-24 16:26:24 +0900

    Time Ago

--- app/src/main/java/com/jhm/android/firebasechat/ChatData.kt
+++ app/src/main/java/com/jhm/android/firebasechat/ChatData.kt
@@ -2,9 +2,11 @@ package com.jhm.android.firebasechat
 
 import com.google.firebase.Timestamp
 
-data class ChatData (
+
+data class ChatData(
     val title: String? = null,
     val password: String? = null,
     val creationTime: Timestamp? = null,
+    val numberOfParticipants: Number? = null,
     val id: String? = null
 )
--- app/src/main/java/com/jhm/android/firebasechat/MessageActivity.kt
+++ app/src/main/java/com/jhm/android/firebasechat/MessageActivity.kt
@@ -6,13 +6,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_message.*
 
 class MessageActivity : AppCompatActivity() {
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
-
+        
         text_message_id.text = intent.getStringExtra("id")
-
+        
         recycler_message_message.apply {
             this.layoutManager = LinearLayoutManager(this@MessageActivity)
             // this.adapter = MessageAdapter(chats)
