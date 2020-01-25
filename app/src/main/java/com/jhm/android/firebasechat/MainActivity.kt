cf27e4059d1a7d25c78e5d74dd10e5a3f265bc38
J-HM <als091212@naver.com>
2020-01-25 20:22:35 +0900

    Commit Test 4

--- app/src/main/java/com/jhm/android/firebasechat/MainActivity.kt
+++ app/src/main/java/com/jhm/android/firebasechat/MainActivity.kt
@@ -13,13 +13,13 @@ import com.google.firebase.firestore.QuerySnapshot
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    
     private var chats = ArrayList<ChatData>()
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        
         recycler_main_chat.apply {
             this.layoutManager = LinearLayoutManager(this@MainActivity)
             this.adapter = ChatAdapter(chats) { position ->
@@ -29,19 +29,16 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-
+        
         getChat(
             onSuccess = { updateRecycler(it) },
             onFailure = { handleException(it) }
         )
     }
-
-    private fun getChat(
-        onSuccess: (QuerySnapshot) -> Unit,
-        onFailure: (FirebaseFirestoreException.Code) -> Unit
-    ) {
+    
+    private fun getChat(onSuccess: (QuerySnapshot) -> Unit, onFailure: (FirebaseFirestoreException.Code) -> Unit) {
         val db = FirebaseFirestore.getInstance()
-
+        
         db.collection("CHAT")
             .orderBy("creationTime")
             .addSnapshotListener { snapshot, exception ->
@@ -55,11 +52,11 @@ class MainActivity : AppCompatActivity() {
                 }
             }
     }
-
+    
     private fun handleException(exceptionCode: FirebaseFirestoreException.Code) {
         Log.w("jhmlog", "GET POST FAILURE: $exceptionCode")
     }
-
+    
     private fun updateRecycler(snapshot: QuerySnapshot) {
         Log.d("jhmlog", "GET POST SUCCESS: ${snapshot.size()}")
         for (document in snapshot.documentChanges) {
@@ -71,36 +68,34 @@ class MainActivity : AppCompatActivity() {
                         if (oldIndex != newIndex) {        // When index of the modified data has changed
                             removeChat(oldIndex)           // Remove the data was in the original index,
                             addChat(updatedChat, newIndex) // Add data to the new index
-                        } else {
-                            modifyChat(updatedChat, newIndex)
-                        }
+                        } else modifyChat(updatedChat, newIndex)
                     }
                     DocumentChange.Type.REMOVED -> removeChat(oldIndex)
                 }
             }
         }
     }
-
+    
     private fun extractChatDataFrom(document: DocumentChange): ChatData {
         val title: String? = document.document.data["title"] as String?
         val password: String? = document.document.data["password"] as String?
-        val creationTime: Timestamp? = document.document.data["time"] as Timestamp?
+        val creationTime: Timestamp? = document.document.data["creationTime"] as Timestamp?
+        val numberOfParticipants: Number? = document.document.data["numberOfParticipants"] as Number?
         val id: String = document.document.id
-
-        return ChatData(title, password, creationTime, id)
+        
+        return ChatData(title, password, creationTime, numberOfParticipants, id)
     }
-
-
+    
     private fun addChat(updatedChat: ChatData, IndexToBeAdd: Int) {
         chats.add(IndexToBeAdd, updatedChat)
         recycler_main_chat.adapter?.notifyItemInserted(IndexToBeAdd)
     }
-
+    
     private fun modifyChat(updatedChat: ChatData, IndexToBeModify: Int) {
         chats[IndexToBeModify] = updatedChat
         recycler_main_chat.adapter?.notifyItemChanged(IndexToBeModify)
     }
-
+    
     private fun removeChat(IndexToBeRemove: Int) {
         chats.removeAt(IndexToBeRemove)
         recycler_main_chat.adapter?.run {
@@ -108,5 +103,5 @@ class MainActivity : AppCompatActivity() {
             notifyItemRangeChanged(IndexToBeRemove, chats.size - IndexToBeRemove)
         }
     }
-
+    
 }
